eff73cdcdb45fb401f63c1d7e2e399965a3c13fa
erica ross <ergerica76@gmail.com>
2019-05-24 23:37:07 -0400

    Add files via upload

---
+++ Kata1Day1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+namespace HelloWorld2
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			using (StreamReader file = new StreamReader ("textFile")) 
+			{
+				int counter = 0;
+				string ln;
+				while ((ln = file.ReadLine ()) != null) 
+				{
+					Console.WriteLine (ln);
+					counter++;
+				}
+
+			}
+		}
+	}
+}
+	
